7f76471dd991ec5321610795c5a6b894c32cabcb
ruslanbulgar <rbulgar@gmail.com>
2024-07-18 11:59:05 +0300

    First Commit

---
+++ src/main/java/com/rbcode/rbc_kafka_msg/RbcKafkaMsgApplication.java
@@ -0,0 +1,13 @@
+package com.rbcode.rbc_kafka_msg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbcKafkaMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbcKafkaMsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbcode/rbc_kafka_msg/RbcKafkaMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbcode.rbc_kafka_msg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbcKafkaMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
